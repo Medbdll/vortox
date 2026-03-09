f3304a21b339501abc6856aea3040b4a6e65ec36
mohamed boudlal <mohamed.boudlal20@ump.ac.ma>
2026-03-09 10:28:19 +0000

    AWEC-59-proteger-lapplication-et-appliquer-la-logique-metier

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,12 @@ cd /var/www
 
 php artisan migrate --force
 php artisan db:seed --force
+sudo chown -R www-data:www-data storage bootstrap/cache
+sudo chmod -R 775 storage bootstrap/cache
 
+npm install
+npm run build
+
+php artisan optimize:clear
 php-fpm
 apache2-foreground
